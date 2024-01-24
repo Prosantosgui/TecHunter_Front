0f79411f8cc6e3e82a6b069dd5549668433f7e97
Guilherme dos Santos <prfsantosgui546@gmail.com>
2024-01-23 23:49:04 -0300

    Landing Page

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+  public logoImgPath = "../../assets/img/logo3_nobg.png"
+  public logoImgAlt = "Imagem logotipo escrito TECHUNTER"
+  public mainImgPath = "../../assets/img/hand-shake.jpg"
+}
