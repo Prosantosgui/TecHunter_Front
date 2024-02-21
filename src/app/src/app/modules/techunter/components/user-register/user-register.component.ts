2862dd6240f118e291a6c11df86460793534c97e
Guilherme dos Santos <prfsantosgui546@gmail.com>
2024-02-21 15:15:18 -0300

    feat: Component formulário de registro

--- src/app/src/app/modules/techunter/components/user-register/user-register.component.ts
+++ src/app/src/app/modules/techunter/components/user-register/user-register.component.ts
@@ -1,12 +1,17 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './user-register.component.html',
   styleUrl: './user-register.component.scss'
 })
 export class UserRegisterComponent {
 
+  public submitForm(form: NgForm) {
+    console.log(form.value);
+  }
 }
